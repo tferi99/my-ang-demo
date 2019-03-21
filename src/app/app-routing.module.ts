08216dc96e1082258418c9a1febfcfda26a6b320
tferi99 <toth_ferenc@hotmail.com>
2019-03-21 10:36:21 +0100

    auto push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
-import {LodashPageComponent} from './lodash-page/lodash-page.component';
-import {RxjsPageComponent} from './rxjs-page/rxjs-page.component';
 import {GridsterPageComponent} from './gridster-page/gridster-page.component';
+import {MainComponent as EventBroadcastMain} from './event-broadcast/main/main.component';
+import {MainComponent as LodashMain} from './lodash/main/main.component';
+import {MainComponent as RxjsMain} from './rxjs/main/main.component';
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  { path: 'lodash', component: LodashPageComponent },
-  { path: 'rxjs', component: RxjsPageComponent },
+  { path: 'broadcast', component: EventBroadcastMain },
+  { path: 'lodash', component: LodashMain },
+  { path: 'rxjs', component: RxjsMain },
   { path: 'gridster', component: GridsterPageComponent},
   { path: '',   redirectTo: '/home', pathMatch: 'full' },   // default
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { LodashPageComponent } from './lodash-page/lodash-page.component';
 import { RxjsPageComponent } from './rxjs-page/rxjs-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { GridsterModule } from 'angular-gridster2';
 import { GridsterPageComponent } from './gridster-page/gridster-page.component';
+import { EventBroadcastModule } from './event-broadcast/event-broadcast.module';
+import { LodashModule } from './lodash/lodash.module';
+import {RxjsModule} from './rxjs/rxjs.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LodashPageComponent,
     RxjsPageComponent,
     HomePageComponent,
     GridsterPageComponent
@@ -24,7 +25,9 @@ import { GridsterPageComponent } from './gridster-page/gridster-page.component';
     AppRoutingModule,
     NgbModule.forRoot(),
     FontAwesomeModule,
-    GridsterModule
+    EventBroadcastModule,
+    LodashModule,
+    RxjsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
