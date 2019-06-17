5b7a0127cc6bfcb5d9fd0c434a2ec989cfa3d8da
tferi99 <toth_ferenc@hotmail.com>
2019-06-17 06:19:26 +0200

    .

--- src/app/ngrx/course-list/course-list.component.ts
+++ src/app/ngrx/course-list/course-list.component.ts
@@ -1,24 +1,51 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {AuthState} from '../auth.reducer';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {Course} from '../../shared/model/course.model';
 import {selectAllCourses} from '../course.selectors';
 import {AllCoursesRequested} from '../course.actions';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'ngrx-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.sass']
 })
-export class CourseListComponent implements OnInit {
+export class CourseListComponent implements OnInit, OnDestroy {
   courses$: Observable<Course[]>;
+  coursesHere: Course[];
+  coursesHereDeep: Course[];
+  coursesHereSub: Subscription;
 
   constructor(private store: Store<AuthState>) {}
 
   ngOnInit() {
     this.store.dispatch(new AllCoursesRequested());
-
     this.courses$ = this.store.pipe(select(selectAllCourses));
+
+    // don't use this way
+    this.coursesHereSub = this.store.pipe(select(selectAllCourses)).subscribe(
+      courses => {
+        this.coursesHere = courses;
+        this.coursesHereDeep = _.cloneDeep(courses);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.coursesHereSub.unsubscribe();
+  }
+
+  badChange() {
+    // YOU CANNOT CHANGE STATE DIRECTLY
+    this.coursesHere[0].description = 'Abc...';
+    console.log('Done.');
+  }
+
+  goodChange() {
+    // YOU CAN CHANGE DEEP COPY OF STATE DIRECTLY
+    this.coursesHereDeep[0].description = 'Abc...';
+    console.log('Done.');
   }
 }
